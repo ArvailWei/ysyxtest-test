fc18cfaec039a5d072a12878d92854fa3365ead9
404-Awei <2445254698@qq.com>
2024-11-19 23:12:02 +0800

    commit

--- ex4/LFshift_reg/csrc/main.cpp
+++ ex4/LFshift_reg/csrc/main.cpp
@@ -8,22 +8,23 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
  
+ //实现clk功能
 static void single_cycle(){
 	dut.clk = 0; dut.eval();
 	dut.clk = 1; dut.eval();
 }
  
 static void reset(int n){
-	dut.rst = 1;
-	while(n-->0) single_cycle();
-	dut.rst = 0;
+	dut.rst = 1;//复位信号 rst=1 进行复位
+	while(n-->0) single_cycle();//经过n个周期
+	dut.rst = 0;//完成复位
 }
  
 int main(){
-	nvboard_bind_all_pins(&dut);
-	nvboard_init();
+	nvboard_bind_all_pins(&dut);//引脚绑定
+	nvboard_init();//初始化
 	
-	reset(10);
+	reset(10);//10个周期复位（#10）稳定系统
 	while(1){
 		nvboard_update();
 		single_cycle();
